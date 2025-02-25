929c32af464c6f394d2bb55f571304783ab50423
Atulchaurasiya-36 <atulchaurasiya66602@gmail.com>
2025-02-26 02:20:01 +0530

    Variables

---
+++ variables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Declaring a variable
+    var name string = "Atul Chaurasiya"
+    fmt.Println(name)
+
+		// infer->without typing data type
+		var class="B.Tech"
+		fmt.Println(class)
+
+		// shortHand syntax
+		roll:=101;
+		fmt.Println(roll)
+
+}
