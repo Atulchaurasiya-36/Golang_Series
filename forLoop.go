1b7da992f48bead70709194265f589502e6f46a3
Atulchaurasiya-36 <atulchaurasiya66602@gmail.com>
2025-02-27 23:16:16 +0530

    for loop

---
+++ forLoop.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	// for loop
+	// for i:=0;i<=3;i++{
+	// 	fmt.Println(i)
+	// }
+
+	// range in for loop
+	for i:=range 11{
+		fmt.Println(i)
+	}
+
+
+}
